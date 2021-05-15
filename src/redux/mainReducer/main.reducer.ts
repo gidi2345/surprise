ba0db87c5714785bf96848b47f8e429d8e021700
Or Stone <OrStone1111@gmail.com>
2021-05-15 23:15:49 +0300

    merge

--- src/redux/mainReducer/main.reducer.ts
+++ src/redux/mainReducer/main.reducer.ts
@@ -23,8 +23,7 @@ export const INITIAL_STATE: INITIAL_STATE_TYPE = {
         {key: 2, src: "https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270", show: 0},
         {key: 3, src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg", show: 0}],
     showStories: false,
-    startStoryKeyToShow : 1,
-    choosenProducts: { uuid: 'install-uuid-egg1', price: 10, imageSrc: Small_Egg, cssProperties: {width: 250, height: 250}}
+    startStoryKeyToShow : 1
 };
 
 const mainReducer: any = (state: INITIAL_STATE_TYPE = INITIAL_STATE, action : any) => {
